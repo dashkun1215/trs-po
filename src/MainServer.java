894f7a9b9aa1b339a6349a2890ed8c5a7e2885d7
Dasha Svynarchuk <daryna.svyarchuk@openexc.com>
2022-06-06 21:50:41 +0300

    123

--- src/MainServer.java
+++ src/MainServer.java
@@ -1,10 +1,11 @@
 public class MainServer {
 
-    public void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Server is up");
         for(int i=0; i<100000; i++){
             int a=(2+4)/3265;
         }
+        Thread.sleep(12534125);
     }
 }
 
